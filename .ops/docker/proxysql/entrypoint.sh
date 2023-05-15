3f4a07e0e61000f185e748d76e1b484c91fbdffc
bezlyudnev.d <dbezliudnev@gmail.com>
2023-05-15 11:50:59 +0300

    add sysbench for test db add exporter metrics 6070 add connection mysql users

--- .ops/docker/proxysql/entrypoint.sh
+++ .ops/docker/proxysql/entrypoint.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-# Генерируем конфигурационный файл proxysql.cnf с использованием значений окружения
-#envsubst < /proxysql.cnf.tpl > /etc/proxysql.cnf
+# Замените эти значения на реальные значения для вашей MySQL базы данных
+MYSQL_HOST="127.0.0.1"
+MYSQL_PORT="3306"
+MYSQL_USER="web_news_user"
+MYSQL_PASSWORD="password"
+MYSQL_DB="web_news"
 
 # Проверяем конфигурацию ProxySQL
 proxysql --check-config -f /etc/proxysql.cnf
+# Подготовка базы данных sysbench
+sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-host=$MYSQL_HOST --mysql-port=$MYSQL_PORT --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASSWORD --mysql-db=$MYSQL_DB --db-driver=mysql --tables=10 --table-size=1000 prepare
 
-# Запускаем ProxySQL в переднем плане
+# Запуск ProxySQL
 exec proxysql -f -c /etc/proxysql.cnf
